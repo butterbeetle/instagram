0483196c5d6cee90a2873cff55c7e70207497647
butterbeetle <butterbeetle@naver.com>
2023-07-31 22:17:32 +0900

    Feat. 팔로잉한 사람 표시

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,8 +1,32 @@
 "use client";
+import { DetailUser } from "@/model/user";
+import Link from "next/link";
+import { PropagateLoader } from "react-spinners";
 import useSWR from "swr";
+import Avatar from "./Avatar";
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR("/api/me");
-  // 4. 클라이언트 컴포넌트에서 followings의 정보를 UI에 보여줌 (Image, username)
-  return <p>FollowingBar</p>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
+  const users = data?.following;
+
+  return (
+    <section>
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ul>
+          {users.map(({ image, username }) => (
+            <li key={username}>
+              <Link href={`/user/${username}`}>
+                <Avatar image={image} highlight />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
