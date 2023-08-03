73529a80c3cdc5382198eac02d325502df8d8d17
butterbeetle <butterbeetle@naver.com>
2023-08-03 22:03:42 +0900

    Feat. 사용자포스트

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,3 +1,4 @@
+import UserPosts from "@/components/UserPosts";
 import UserProfile from "@/components/UserProfile";
 import { getUserForProfile } from "@/service/user";
 import { notFound } from "next/navigation";
@@ -12,5 +13,10 @@ export default async function UserPage({ params: { username } }: Props) {
     notFound();
   }
 
-  return <UserProfile user={user} />;
+  return (
+    <>
+      <UserProfile user={user} />
+      <UserPosts user={user} />
+    </>
+  );
 }
