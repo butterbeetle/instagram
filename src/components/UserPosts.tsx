1e82bd2b70a9b2346e33aff9f5431c886f9e07f3
butterbeetle <butterbeetle@naver.com>
2023-08-03 22:16:32 +0900

    Feat. 사용자 포스트

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -2,22 +2,36 @@
 
 import { ProfileUser } from "@/model/user";
 import { useState } from "react";
-import useSWR from "swr";
+import PostIcon from "./ui/icons/PostIcon";
+import BookmarkIcon from "./ui/icons/BookmarkIcon";
+import HeartIcon from "./ui/icons/HeartIcon";
+import PostGrid from "./PostGrid";
 
 type Props = {
   user: ProfileUser;
 };
+const tabs = [
+  { type: "posts", icon: <PostIcon /> },
+  { type: "saved", icon: <BookmarkIcon className="w-3 h-3" /> },
+  { type: "liked", icon: <HeartIcon className="w-3 h-3" /> },
+];
 export default function UserPosts({ user: { username } }: Props) {
   // /api/users/${username}/posts
   // /api/users/${username}/liked
   // /api/users/${username}/bookmarks
-  const [tab, setTab] = useState("posts");
-  const {
-    data: posts,
-    isLoading,
-    error,
-  } = useSWR(`/api/users/${username}/${tab}`);
-  console.log(posts);
+  const [query, setQuery] = useState(tabs[0].type);
 
-  return <></>;
+  return (
+    <section>
+      <ul>
+        {tabs.map(({ type, icon }) => (
+          <li key={type} onClick={() => setQuery(type)}>
+            <button>{icon}</button>
+            <span>{type}</span>
+          </li>
+        ))}
+      </ul>
+      <PostGrid username={username} query={query} />
+    </section>
+  );
 }
