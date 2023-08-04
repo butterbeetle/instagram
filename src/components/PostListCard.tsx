cc9ed6d8a2b314c5f668660d697caeef489f9aa0
butterbeetle <butterbeetle@naver.com>
2023-08-04 22:35:14 +0900

    Feat. 코멘트 기능 - 홈 화면

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -10,14 +10,20 @@ import ModalPortal from "./ui/ModalPortal";
 import PostModal from "./PostModal";
 import PostDetail from "./PostDetail";
 import PostUserAvatar from "./PostUserAvatar";
+import usePosts from "@/hooks/posts";
 
 type Props = {
   post: SimplePost;
   priority?: boolean;
 };
 export default function PostListCard({ post, priority = false }: Props) {
-  const { userImage, username, image, createdAt, likes, text } = post;
+  const { userImage, username, image, comments, text } = post;
   const [openModal, setOpenModal] = useState(false);
+  const { postComment } = usePosts();
+
+  const handlePostComment = (comment: string) => {
+    postComment(post, comment);
+  };
 
   return (
     <article className="rounded-lg shadow-md border-gray-200">
@@ -31,8 +37,19 @@ export default function PostListCard({ post, priority = false }: Props) {
         height={500}
         onClick={() => setOpenModal(true)}
       />
-      <ActionBar post={post} />
-      <CommentForm />
+      <ActionBar post={post}>
+        <p>
+          <span className="font-bold mr-1">{username}</span>
+          {text}
+        </p>
+        {comments > 1 && (
+          <button
+            className="font-bold my-2 text-sky-500"
+            onClick={() => setOpenModal(true)}
+          >{`View all ${comments} comments`}</button>
+        )}
+      </ActionBar>
+      <CommentForm onPostComment={handlePostComment} />
       {openModal && (
         <ModalPortal>
           <PostModal onClose={() => setOpenModal(false)}>
