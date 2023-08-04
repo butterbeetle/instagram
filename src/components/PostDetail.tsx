e55a6e658dc83dbe78aaa9b5cf3e46c2e08ca8c2
butterbeetle <butterbeetle@naver.com>
2023-08-04 22:43:29 +0900

    Feat. 코멘트 상세화면

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -5,15 +5,25 @@ import PostUserAvatar from "./PostUserAvatar";
 import ActionBar from "./ActionBar";
 import CommentForm from "./CommentForm";
 import Avatar from "./Avatar";
+import useFullPost from "@/hooks/post";
+import useMe from "@/hooks/me";
 type Props = {
   post: SimplePost;
 };
 export default function PostDetail({ post }: Props) {
-  const { id, userImage, username, image, createdAt, likes } = post;
-  const { data } = useSWR<FullPost>(`/api/posts/${id}`);
+  const { id, userImage, username, image } = post;
+  const { post: data, postComment } = useFullPost(id);
+  const { user } = useMe();
   const comments = data?.comments;
 
-  const handlePostComment = (comment: string) => {};
+  const handlePostComment = (comment: string) => {
+    user &&
+      postComment({
+        comment,
+        username: user.username,
+        image: user.image,
+      });
+  };
 
   return (
     <section className="flex w-full h-full">
