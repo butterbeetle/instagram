f9acc2981014b41f0bd1698ead32d1f7a09ecfde
butterbeetle <butterbeetle@naver.com>
2023-08-07 00:33:56 +0900

    Feat. 새 포스트 - server

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
@@ -14,3 +15,5 @@ const builder = imageUrlBuilder(client);
 export function urlFor(source: SanityImageSource) {
   return builder.image(source).width(800).url();
 }
+
+export const assetsURL = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v2021-06-07/assets/images/${process.env.SANITY_DATASET}`;
